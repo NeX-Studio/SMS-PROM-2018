7c8f9b4b6959715b8a2a48ef21b233a1a8b4a5ad
William-An <china_aisa@live.com>
2018-04-14 23:30:38 +0800

    Update data structure && adding registration logic

--- data_structure.js
+++ data_structure.js
@@ -1,17 +1,21 @@
 // POST https://prom.thexyzlab.tech/register
-var info = [
-    {
-        "name": "",
-        "gender": "",
-        "type": "", // Student, Parent, Child, and Partner
-        "group": "", // Enter student name, for grouping student, parents, and children from same family
+var info = {
+    "meta": {
         // Randomly Generated, will be sent to the student phone number for updating info
         // If the group existed with different uuid, this post will be considered unauthorized 
         "uuid": "", 
-        "tel": "",
-        "avoidance": "",
-        // Student and Partner Only Options
-        // Empty string for partner indicates that he/she is not in the international curriculum; thus whose fee must also pay by the student
-        "class": ""
+        "group": "", // Student name, for grouping student, parents, and children from same family
+        "avoidance": ""
     }
-]
+    "participants": [
+        {
+            "name": "",
+            "gender": "",
+            "type": "", // Student, Parent, Child, and Partner
+            "tel": "",
+            // Student and Partner Only Options
+            // Empty string for partner indicates that he/she is not in the international curriculum; thus whose fee must also pay by the student
+            "class": ""
+        }
+    ]
+}
--- routes/register.js
+++ routes/register.js
@@ -11,16 +11,73 @@ router.post('/', function(req, res, next) {
 		let client;
 		try{
 			// Sterilize Input Data
-			
+			request = request.participants.map(sterilizeData, request.meta);
+			let fee = getFee(request.participants);
+			let uuid = request.meta.uuid
+			let group = request.meta.group
 			client = await MongoClient.connect(MONGO_URL);
 			const db = client.db(DB_NAME);
-			// Insert documents
-			let cursor = await db.collection('participants');
+			let cursor = await db.collection('participants').findOne({"uuid": uuid});
+			if (cursor){
+				// Update Documents
+				cursor = await db.collection('participants').deleteMany("uuid": uuid);
+				cursor = await db.collection('participants').insertMany(request);
+				res.status().json({errcode: 0, errmsg: "", class: request.class, fee: fee})
+			}
+			else {
+				cursor = await db.collection('participants').findOne({"group": group});
+				// If attempts to add to same group without authorization happen
+				if (cursor.uuid != uuid)
+					res.status(401).json({errorcode: 10005, errmsg: 'Invalid uuid'});
+				else{
+					// Insert documents
+					 = await db.collection('participants').insertMany(request);
+					res.status(201).json({errcode: 0, errmsg: ""});
+				}
+			}
 		} catch(err){
 			console.log(err.stack);
 			next(err);
 		}
 	});
 });
+
+function sterilizeData(val){
+	// Use 'this' to get meta info
+	return {
+		"name": typeof val.name == "string" ? val.name : "",
+		"gender": typeof val.gender == "string" ? val.gender : "",
+		"type": typeof val.type == "string" ? val.type : "",
+		"group": typeof this.group == "string" ? this.group : "",
+		"uuid": typeof this.uuid == "string" ? this.uuid : "",
+		"tel": typeof val.tel == "string" ? val.tel : "",
+		"avoidance": typeof this.avoidance == "string" ? this.avoidance : "",
+		"class": typeof val.class == "string" ? val.class : ""
+	}
+}
+
+function getFee(arr){
+	let fee = 0;
+	for(let i = 0; i < arr.length; i++){
+		switch(arr[i].type){
+			case "parent":
+				fee += 350;
+				break;
+			case "child":
+				fee += 150;
+				break;
+			case "partner":
+				if(arr[i].class == "")
+					fee += 400;
+				break;
+			case "student":
+				fee += 400;
+				break;
+			default:
+				fee += 0;
+		}
+	}
+	return fee
+}
 
 module.exports = router;
